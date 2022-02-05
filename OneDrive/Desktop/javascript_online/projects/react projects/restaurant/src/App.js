8ad5b608829bf91ede37107424e8c57677960f17
istiak2426 <you@example.com>
2022-02-05 19:47:44 +0600

    Component tree

---
+++ OneDrive/Desktop/javascript_online/projects/react projects/restaurant/src/App.js
@@ -0,0 +1,18 @@
+import MainComponent from "./components/pages/main/MainComponent";
+
+
+
+
+
+const App = () => {
+
+  
+  return <div>
+
+<MainComponent/>
+
+  
+    </div>;
+};
+
+export default App;
